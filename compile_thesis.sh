c185e19d60bc9389f4ef9bfbcef364f193347f11
Emmanuel DERVIEUX <emmanueldervieux@biosency.com>
2024-10-16 16:37:21 +0200

    compilation toolchain finished and aparently working

--- compile_thesis.sh
+++ compile_thesis.sh
@@ -5,6 +5,8 @@
 set -e
 
 m_verbose="no"
+m_fig_compile="no"
+m_man_compile="no"
 
 function Help()
 {
@@ -13,19 +15,23 @@ echo "Hoopefully compiles the thesis' manuscript."
 echo
 echo "Syntax: scriptTemplate [-h|v]"
 echo "options:"
+echo "f     Compile TikZ figures"
 echo "h     Print this Help."
 echo "v     Verbose mode."
 echo
 }
 
-while getopts ":hv" option; do
+while getopts ":fhmv" option; do
 	case $option in
+		f) # compile figures
+			m_fig_compile="yes";;
 		h) # display Help
 			Help
 			exit;;
+		m) # compile full manuscript
+			m_man_compile="yes";;
 		v) # verbose
-			m_verbose="yes"
-			;;
+			m_verbose="yes";;
 		\?) # Invalid 
 			echo "Error: Invalid option"
 			exit;;
@@ -36,24 +42,29 @@ done
 # Compile figures for each part separately
 ####################################################
 
-source ./compilation_chain/0_front_matter.sh
-source ./compilation_chain/1_main_matter.sh
-source ./compilation_chain/2_appendices.sh
-source ./compilation_chain/3_back_matter.sh
+if [ $m_fig_compile = "yes" ]; then
+	source ./compilation_chain/0_front_matter.sh
+	source ./compilation_chain/1_main_matter.sh
+	source ./compilation_chain/2_appendices.sh
+	source ./compilation_chain/3_back_matter.sh
+fi
 
 ####################################################
 # Main compilation
 ####################################################
 
-# Creating main output dirs
-mkdir -p out
-mkdir -p out/0_front_matter
-mkdir -p out/1_main_matter
-mkdir -p out/2_appendices
-mkdir -p out/3_back_matter
+if [ $m_man_compile = "yes" ]; then
+	# Creating main output dirs
+	mkdir -p out
+	mkdir -p out/0_front_matter
+	mkdir -p out/1_main_matter
+	mkdir -p out/2_appendices
+	mkdir -p out/3_back_matter
 
-# Main compilation
-#lualatex -synctex=1 -output-directory out -interaction=nonstopmode thesis.tex
-#(cd out && makeglossaries thesis)
-#biber --output-directory out thesis
-#lualatex -synctex=1 -output-directory out -interaction=nonstopmode thesis.tex
+	# Main compilation
+	lualatex -synctex=1 -output-directory out -interaction=errorstopmode thesis.tex
+	(cd out && makeglossaries thesis)
+	biber --output-directory out thesis
+	lualatex -synctex=1 -output-directory out -interaction=errorstopmode thesis.tex
+	lualatex -synctex=1 -output-directory out -interaction=errorstopmode thesis.tex
+fi
