b63f263da4d0a7d3ac781a40631f682b4098a589
Emmanuel DERVIEUX <emmanueldervieux@biosency.com>
2024-10-17 07:43:19 +0200

    add ghostscript compression to compilation script

--- compilation_chain/1_main_matter.sh
+++ compilation_chain/1_main_matter.sh
@@ -5,7 +5,11 @@ source $DIR/complib.sh
 
 echo ---- Generating main matter figures ----
 cd 1_main_matter
-for location in choos_figures/review/tikz choos_figures/schemes/tikz co2hb_figures/tikz; do
-	comptikz $location
+for location in choos_figures/review choos_figures/schemes co2hb_figures conclusion_figures intro_figures tcco2_figures thin_film_figures/encasp_pbl thin_film_figures/experimentals thin_film_figures/luminophores thin_film_figures/optoelectronics; do
+	comptikz $location/tikz
 done
 cd ..
+
+FILE="1_main_matter/tcco2_figures/tikz/out/photo_pos.pdf"
+FILE_NAME=${FILE%.*}
+gs -sDEVICE=pdfwrite -dMaxSubsetPct=100 -dPDFSETTINGS=/printer -sOutputFile="${FILE_NAME}_comp.pdf" -sColorConversionStrategy='LeaveColorUnchanged' -dNOPAUSE -dBATCH $FILE
